df6b89b43ae09253b35e3674e93dcb1526235867
Mike Miller <83832694+mikemiller97@users.noreply.github.com>
2023-02-10 00:41:38 +0000

    Mat plot lib line graphs

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
-import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import numpy as np
-import datetime as DT
 
-df = pd.read_csv('https://raw.githubusercontent.com/cwkteacher/Data/master/athletes.csv')
+#prepare data
+linear_data = np.array([1, 4, 2 , 5])
 
-# Nations value
-#print(df['nationality'].value_counts()[:30])
+#plot figure
+fig = plt.figure()
+plt.plot(linear_data, '-o')
 
-#print total medals
-gold = df.groupby('nationality').sum()['gold']
-silver = df.groupby('nationality').sum()['silver']
-bronze = df.groupby('nationality').sum()['bronze']
+#save file
+fig.savefig('line.png')
 
-total_medals = gold + silver + bronze
-
-#print(total_medals)
-#print(gold['IND'])
-
-#Age now
-#now = pd.Timestamp(DT.datetime.now())
-
-#Age as they were in 2016!!
-now = pd.Timestamp(DT.datetime(2016, 10, 5, 18, 00))
-
-df['dob'] = pd.to_datetime(df['dob'])
-df['dob'] = df['dob'].where(df['dob'] < now, df['dob'] - np.timedelta64(100, 'Y'))
-df['age'] = (now - df['dob']).astype('<m8[Y]')
-print(df)
-
-print("Something to make this different.")
